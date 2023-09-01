01a5cbfecd39b781479a0d81cb49d3131c73905b
Abbas Abdallah <abbasab13@outlook.com>
2023-09-01 16:14:55 +0300

    updated

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "e0e78dbee4msh0d3f4893ec6d209p17051fjsn0ab555d1e57e",
+      "X-RapidAPI-Key": "502e226d9amshc53392167be5a83p1e2cfcjsn4655e2fb567b",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
