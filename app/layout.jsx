bfab601db44dad2d14303c1dba392b31cc8761bd
Abbas Abdallah <abbasab13@outlook.com>
2023-09-01 16:33:15 +0300

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ import Footer from '@/components/Footer'
 
 export const metaData = {
     title: 'Elite Estates',
-    description: 'Find the perfect house that suits your needs in the UAE'
+    description: 'Find the perfect house that suits your needs in the UAE',
 }
 
 export const Root = ({children}) => {
